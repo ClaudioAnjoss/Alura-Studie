cc9199626c658209ca4c7f4746a0f9daab2bd3fd
Claudio Anjos <claudio.anjoss@outlook.com>
2023-11-16 13:18:06 -0300

    Criando Cronometro
    Adicionar informações externas ao componente com Props;
    Aprendemos a utilizar propriedades externas para reutilizar componentes de forma única.
    Funciona o props.children;
    Vimos que o React disponibiliza uma props padrão chamada children, que ao contrário das outras props, é escrita dentro do componente, abrindo e fechando a tag como em uma tag HTML.
    O key (chaves) é importante em arrays no React;
    Entendemos mais a fundo como o React trabalha com arrays, e como o key é importante para que ele não se perca entre os itens deste array.
    Usar spread operator de forma consciente para passar props para os componentes;
    Utilizamos o spread operator para poder passar todos os itens de um objeto como props para o componente, também pensamos como isso pode se tornar um problema.

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,12 +1,7 @@
 import style from './Item.module.scss'
 
-export default function Item({
-  tarefa,
-  tempo,
-}: {
-  tarefa: string
-  tempo: string
-}) {
+export default function Item(props: any) {
+  const { tarefa, tempo } = props
   return (
     <li className={style.item}>
       <h2>{tarefa}</h2>
